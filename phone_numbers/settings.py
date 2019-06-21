9b9a4d45e96a19baa9750b37f1c3609a9b26b7da
hugyy85 <46883700+hugyy85@users.noreply.github.com>
2019-06-21 08:43:08 +0700

    Update settings.py

--- phone_numbers/settings.py
+++ phone_numbers/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4_d_&d#o8g098pf47!0_kldur@=qnj)ydeswk%z4u@@#--*3hd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
